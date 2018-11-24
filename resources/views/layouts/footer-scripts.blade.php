a20ccd7500bfcba06b07afd2e6683c3b292e563a
Matt Barba <matthewlawrencebarba@gmail.com>
2018-11-24 07:56:16 -0500

    all the scripts / styles should have thier paths fixed

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,35 +1,35 @@
 <!-- jQuery 3 -->
-<script src="AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
 
 
 <!-- Custom Scripts for the project -->
-<script src="js/helpers/general.js"></script>
-<script src="js/helpers/search-form.js"></script>
-<script src="js/helpers/datatable.js"></script>
-<script src="js/main.js"></script>
-<script src="js/config/listings.js"></script>
-<script src="js/config/zips.js"></script>
-<script src="js/config/watchlists.js"></script>
+<script src="/js/helpers/general.js"></script>
+<script src="/js/helpers/search-form.js"></script>
+<script src="/js/helpers/datatable.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/config/listings.js"></script>
+<script src="/js/config/zips.js"></script>
+<script src="/js/config/watchlists.js"></script>
 
 
 
 
 
 <!-- Bootstrap 3.3.7 -->
-<script src="AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Select2 -->
-<script src="js/helpers/moment.js"></script>
+<script src="/js/helpers/moment.js"></script>
 <!-- Select2 -->
-<script src="AdminLTE/bower_components/select2/dist/js/select2.full.min.js"></script>
+<script src="/AdminLTE/bower_components/select2/dist/js/select2.full.min.js"></script>
 <!-- SlimScroll -->
-<script src="AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="/AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="AdminLTE/dist/js/adminlte.min.js"></script>
+<script src="/AdminLTE/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <!-- <script src="AdminLTE/dist/js/demo.js"></script> -->
 
 <!-- DataTables -->
-<script src="AdminLTE/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="AdminLTE/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="/AdminLTE/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="/AdminLTE/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
