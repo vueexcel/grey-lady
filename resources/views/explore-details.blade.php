c06ba3f8d6b974e31335302eae446557fae17947
Matt Barba <matthewlawrencebarba@gmail.com>
2018-12-01 11:17:30 -0500

    More explore details clean up

--- resources/views/explore-details.blade.php
+++ resources/views/explore-details.blade.php
@@ -28,14 +28,14 @@
     
     </div>
   </div>
-
+{{-- 
   <div class="box box-default">
     <div class="box-body">
 
       Listings datatable
     
     </div>
-  </div>
+  </div> --}}
 
   <div class="col-md-6">
 
@@ -122,7 +122,24 @@
   <div class="box box-default">
     <div class="box-body">
       
-      Actions
+      <h4>Actions</h4>
+
+      <div class="btn-group">
+          <button type="button" class="btn btn-default">API Calls</button>
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+            <span class="caret"></span>
+            <span class="sr-only">Toggle Dropdown</span>
+          </button>
+          <ul class="dropdown-menu" role="menu">
+            <li><a target="_Blank" href="//gulshan.app.greyladyproject.com/api/v1/watchlist?zip={{ $zip[0]->details->zip }}">Search Watchlists</a></li>
+            <li><a target="_Blank" href="//gulshan.app.greyladyproject.com/api/v1/report?zip={{ $zip[0]->details->zip }}">Average Rent</a></li>
+            <li><a target="_Blank" href="//gulshan.app.greyladyproject.com/api/v1/listings?type=sell&amp;zip={{ $zip[0]->details->zip }}">Listing Sell Search</a></li>
+            <li><a target="_Blank" href="//gulshan.app.greyladyproject.com/api/v1/listings?type=rent&amp;zip={{ $zip[0]->details->zip }}">Listing Rent Search</a></li>
+            <li><a target="_Blank" href="//gulshan.app.greyladyproject.com/api/v1/recalculate/listings?zip={{ $zip[0]->details->zip }}">Recalculate Zip Listings</a></li>
+            <li><a target="_Blank" href="//gulshan.app.greyladyproject.com/api/v1/recalculate/zips?zip={{ $zip[0]->details->zip }}">Recalculate Zip</a></li>
+            <li><a target="_Blank" href="//gulshan.app.greyladyproject.com/api/v1/zip?zip={{ $zip[0]->details->zip }}">Zip Details</a></li>
+          </ul>
+        </div>
 
     </div>
   </div>
@@ -256,8 +273,12 @@
 
 </div>
 
-<div class="col-md-8 row"> 
-  {{ var_dump($zip[0]) }}
+<div class="col-md-12 row"> 
+  <div class="box box-default">
+    <div class="box-body">
+      {{ var_dump($zip[0]) }}
+    </div>
+  </div>
 </div>
 
 
