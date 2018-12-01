ab5c974f2a4680dc5278bac9a4cf647fe8beb5d4
Matt Barba <matthewlawrencebarba@gmail.com>
2018-12-01 11:12:29 -0500

    Format all the data on the page

--- resources/views/explore-details.blade.php
+++ resources/views/explore-details.blade.php
@@ -11,7 +11,20 @@
   <div class="box box-default">
     <div class="box-body">
 
-      Beds vs Cap Rate      
+      <h4>Beds vs Cap Rate</h4>
+
+      @if (isset($zip[0]->calculated_fields->by_beds))
+
+        @foreach ($zip[0]->calculated_fields->by_beds as $bed => $bed_zip_stats)
+
+          <div class="precentage">
+            <span class="precentage-top">{{ number_format($bed_zip_stats->average_cap_rate, 2) }}</span>
+            <span class="precentage-bottom">{{ $bed }}b</span>
+          </div>
+        
+        @endforeach
+
+      @endif  
     
     </div>
   </div>
@@ -23,6 +36,83 @@
     
     </div>
   </div>
+
+  <div class="col-md-6">
+
+    <div class="box box-default">
+      <div class="box-body">
+
+        <h4>10 best Cash on Cash</h4> 
+
+        @if (isset($zip[0]->calculated_fields->best_listings->by_cash_on_cash))
+
+          <table>
+            <thead>
+                <tr>
+                    <th>Address</th>
+                    <th>Beds</th>
+                    <th>CoC</th>
+                </tr>
+            </thead>
+            <tbody>
+          @foreach ($zip[0]->calculated_fields->best_listings->by_cash_on_cash as $coc_listing)
+
+              <tr>
+                {{-- <th>{{ $coc_listing->id }}</th> --}}
+                <td>{{ $coc_listing->address }}</td> 
+                <td>{{ $coc_listing->beds }}</td> 
+                <td>{{ number_format($coc_listing->cash_on_cash, 2) }}</td> 
+              </tr>
+          
+          @endforeach
+          </tbody>
+          </table>
+
+        @endif
+      
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col-md-6">
+    <div class="box box-default">
+      <div class="box-body">
+
+        <h4>10 Best Cap Rate</h4> 
+        
+        @if (isset($zip[0]->calculated_fields->best_listings->by_cap_rate))
+
+          <table>
+            <thead>
+                <tr>
+                    <th>Address</th>
+                    <th>Beds</th>
+                    <th>Cap</th>
+                </tr>
+            </thead>
+            <tbody>
+          @foreach ($zip[0]->calculated_fields->best_listings->by_cap_rate as $coc_listing)
+
+              <tr>
+                {{-- <th>{{ $coc_listing->id }}</th> --}}
+                <td>{{ $coc_listing->address }}</td> 
+                <td>{{ $coc_listing->beds }}</td> 
+                <td>{{ number_format($coc_listing->cap_rate, 2) }}</td> 
+              </tr>
+          
+          @endforeach
+          </tbody>
+          </table>
+
+        @endif
+
+      
+      </div>
+    </div>
+
+  </div>
+  
 
 </div>
 
@@ -40,7 +130,59 @@
   <div class="box box-default">
     <div class="box-body">
       
-      Zip Summary
+      <h4>Zip Summary</h4>
+
+{{--   
+          1: {
+          average_price: 133500,
+          average_baths: null,
+          average_sqft: null,
+          average_cap_rate: 0.08293878403744807,
+          average_cash_on_cash: 0.015829370917824454,
+          average_rent_to_value: 0.00921542044860534,
+          average_debt_coverage_ratio: null,
+          average_revenue: null,
+          number_of_listings: 4
+}, --}}
+
+      @if (isset($zip[0]->calculated_fields->by_beds))
+
+        <table>
+          <thead>
+              <tr>
+                  <th>Price</th>
+                  <th>Baths</th>
+                  <th>Sqft</th>
+                  <th>Cap</th>
+                  <th>CoC</th>
+                  <th>r2v</th>
+                  <th>rent</th>
+                  <th># listings</th>
+              </tr>
+          </thead>
+          <tbody>
+
+        @foreach ($zip[0]->calculated_fields->by_beds as $bed => $bed_zip_stats)
+
+          <tr>
+            {{-- <th>{{ $coc_listing->id }}</th> --}}
+            <td>{{ $bed_zip_stats->average_price }}</td> 
+            <td>{{ $bed_zip_stats->average_baths }}</td> 
+            <td>{{ $bed_zip_stats->average_sqft }}</td> 
+            <td>{{ $bed_zip_stats->average_sqft }}</td> 
+            <td>{{ number_format($bed_zip_stats->average_cap_rate, 2) }}</td> 
+            <td>{{ number_format($bed_zip_stats->average_cash_on_cash, 2) }}</td> 
+            <td>{{ number_format($bed_zip_stats->average_rent_to_value, 2) }}</td> 
+            <td>{{ $bed_zip_stats->average_revenue }}</td> 
+            <td>{{ $bed_zip_stats->number_of_listings }}</td> 
+          </tr>
+        
+        @endforeach
+
+        </tbody>
+        </table>
+
+      @endif  
 
     </div>
   </div>
@@ -48,7 +190,21 @@
   <div class="box box-default">
     <div class="box-body">
       
-      Housing
+      <h4>Housing</h4>
+
+      @if (isset($zip[0]->details->housing))
+
+        <ul>
+
+        @foreach ($zip[0]->details->housing as $key => $housing_stat)
+
+          <li><b>{{ $key }}:</b> {{ $housing_stat }}</li>
+        
+        @endforeach
+
+        </ul>
+
+      @endif  
 
     </div>
   </div>
@@ -56,7 +212,21 @@
   <div class="box box-default">
     <div class="box-body">
       
-      Income
+      <h4>Income</h4>
+
+      @if (isset($zip[0]->details->income))
+
+        <ul>
+
+        @foreach ($zip[0]->details->income as $key => $income_stat)
+
+          <li><b>{{ $key }}:</b> {{ $income_stat }}</li>
+        
+        @endforeach
+
+        </ul>
+
+      @endif  
 
     </div>
   </div>
@@ -66,14 +236,31 @@
       
       Education
 
+      @if (isset($zip[0]->details->education))
+
+        <ul>
+
+        @foreach ($zip[0]->details->education as $key => $education_stat)
+
+          <li><b>{{ $key }}:</b> {{ $education_stat }}</li>
+        
+        @endforeach
+
+        </ul>
+
+      @endif  
+
     </div>
   </div>
 
 
 </div>
 
+<div class="col-md-8 row"> 
+  {{ var_dump($zip[0]) }}
+</div>
 
-{{ var_dump($zip[0]) }}
+
 
 
 @endsection
