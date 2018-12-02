00e96b61f1794051cd4bf1b912685aa8c28db4c1
Matt Barba <matthewlawrencebarba@gmail.com>
2018-12-02 11:54:41 -0500

    Formatting

--- resources/views/explore-details.blade.php
+++ resources/views/explore-details.blade.php
@@ -317,12 +317,27 @@
 
 </div>
 
+
+
 <div class="col-md-12 row"> 
-  <div class="box box-default">
-    <div class="box-body">
+
+  <div class="box box-default collapsed-box">
+    <div class="box-header with-border">
+      <h3 class="box-title">Raw json response</h3>
+
+      <div class="box-tools pull-right">
+        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+        </button>
+      </div>
+      <!-- /.box-tools -->
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body" style="display: none;">
       {{ print_r($zip[0]) }}
     </div>
+    <!-- /.box-body -->
   </div>
+
 </div>
 
 
