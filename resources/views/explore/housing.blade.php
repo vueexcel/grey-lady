f92abec99861282b1285c71ba53c702bcfc3e2a0
Matt Barba <matthewlawrencebarba@gmail.com>
2018-12-02 21:51:31 -0500

    A little more formatting

--- resources/views/explore/housing.blade.php
+++ resources/views/explore/housing.blade.php
@@ -1,21 +1,62 @@
-<div class="box box-default">
-  <div class="box-body">
-    
-    <h4>Housing</h4>
+{{-- <div class="row"> --}}
 
-    @if (isset($zip[0]->details->housing))
-
-      <ul>
-
-      @foreach ($zip[0]->details->housing as $key => $housing_stat)
-
-        <li><b>{{ $key }}:</b> {{ $housing_stat }}</li>
+  <div class="box box-default">
+    <div class="box-body">
       
-      @endforeach
+      <h4>Housing</h4>
 
-      </ul>
+      <div class="row">
 
-    @endif  
+        <div class="col-sm-3 col-xs-6">
+          <div class="description-block">
+            <h5 class="description-header">{{ formatLargeNumber($zip[0]->details->housing->population) }}</h5>
+            <span class="description-text">Population</span>
+          </div>
+        </div>
 
+        <div class="col-sm-3 col-xs-6">
+          <div class="description-block">
+            <h5 class="description-header">{{ formatLargeNumber($zip[0]->details->housing->housing_units) }}</h5>
+            <span class="description-text">Housing</span>
+          </div>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="description-block">
+            <h5 class="description-header">{{ formatPercentage($zip[0]->details->housing->occupied_units/$zip[0]->details->housing->housing_units) }}</h5>
+            <span class="description-text">Occupied</span>
+          </div>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="description-block">
+            <h5 class="description-header">{{ formatPercentage($zip[0]->details->housing->renter_occupied_housing_units/$zip[0]->details->housing->housing_units) }}</h5>
+            <span class="description-text">Renter</span>
+          </div>
+        </div>
+
+    </div>
+    <div class="row">
+      
+      <div class="col-sm-3 col-xs-6">
+        <div class="description-block">
+          <h5 class="description-header">{{ formatLargeCurrency($zip[0]->details->income->median_income) }}</h5>
+          <span class="description-text">Med Income</span>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-xs-6">
+        <div class="description-block">
+          <h5 class="description-header">{{ formatLargeCurrency($zip[0]->details->income->mean_income) }}</h5>
+          <span class="description-text">Mean Income</span>
+        </div>
+      </div>
+      
+    </div>
+
+
+    </div>
   </div>
-</div>
+
+{{-- </div> --}}
+
--- resources/views/explore/watchlist.blade.php
+++ resources/views/explore/watchlist.blade.php
@@ -1,6 +1,6 @@
 <div class="box box-default collapsed-box">
   <div class="box-header with-border">
-    <h3 class="box-title">Watchlist</h3>
+    <h3 class="box-title">Dump of Watchlist, Income and Education</h3>
 
     <div class="box-tools pull-right">
       <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
@@ -42,6 +42,22 @@
         - Nothing
 
       @endif  
+
+      <h4>Housing</h4>
+
+      <ul>
+        @foreach ($zip[0]->details->housing as $key => $housing_stat)
+          <li><b>{{ $key }}:</b> {{ $housing_stat }}</li>
+        @endforeach
+      </ul>
+
+      <h4>Income</h4>
+
+      <ul>
+        @foreach ($zip[0]->details->income as $key => $income_stat)
+          <li><b>{{ $key }}:</b> {{ $income_stat }}</li>
+        @endforeach
+      </ul>
   </div>
   <!-- /.box-body -->
 </div>
