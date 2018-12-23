fe0c9b72894e9f92042ad595c9f4e89519b9ebb6
Matt Barba <matthewlawrencebarba@gmail.com>
2018-12-23 08:25:25 +0100

    Support for gravatar for users

--- app/User.php
+++ app/User.php
@@ -28,4 +28,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    
+
+    public function getGravatarAttribute() {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash";
+    }
+
 }
