a4b1c420fda8f04b398bcd52dcf78230a66cba83
Matt Barba <matthewlawrencebarba@gmail.com>
2018-12-24 09:37:09 +0100

    seems like we're just guessing now..

--- database/migrations/2018_12_22_142823_favorite_listings.php
+++ database/migrations/2018_12_22_142823_favorite_listings.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class favorite_listings extends Migration
+class FavoriteListings extends Migration
 {
     /**
      * Run the migrations.
@@ -17,6 +17,7 @@ class favorite_listings extends Migration
 
         Schema::create('favorite_listings', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
             $table->string('listing_id');
             $table->text('listing_address');
             $table->timestamps();
