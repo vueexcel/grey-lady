53ebc6178035df812995c065ee540bae05676229
Matt Barba <matthewlawrencebarba@gmail.com>
2018-12-30 15:17:35 +0100

    Note to self, modifying the user table is pain in the ass

---
+++ database/migrations/2018_12_30_141618_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class User extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function($table) {
+            $table->dropColumn('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('users', function($table) {
+        //     $table->string('phone');
+        // });
+    }
+}
