8de396065d98bc9c40f16de23a7400beda0c8f41
Matt Barba <matthewlawrencebarba@gmail.com>
2019-01-15 18:17:05 -0500

    sometimes source pdp is blank

--- resources/views/listings/details/source.blade.php
+++ resources/views/listings/details/source.blade.php
@@ -2,12 +2,15 @@
 	<div class="box-body">
 		<h4>Found on:</h4>
 
-		@if ( isset($listing->source->source) )
 			<p>{{ $listing->source->source }} - 
-				<a target=_blank href="{{ $listing->source->source_pdp }}">PDP Link</a> /
-				<a target=_blank href="{{ $listing->source->source_link }}">Found on</a>
-			</p>
-		@endif
 
+				@if ( isset($listing->source->source_pdp) )
+					<a target=_blank href="{{ $listing->source->source_pdp }}">PDP Link</a> /	
+				@endif	
+
+				@if ( isset($listing->source->source_link) )
+					<a target=_blank href="{{ $listing->source->source_link }}">Found on</a>
+				@endif	
+			</p>
 	</div>
 </div>
