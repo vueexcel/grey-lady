928475ea35cad2b837ad4683726827181a63aac3
Matt Barba <matthewlawrencebarba@gmail.com>
2019-01-16 19:32:24 -0500

    Minor formatting change to handle smaller rents

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -19,19 +19,12 @@ function formatPercentage($number_to_format, $places_to_format = false) {
 
 function formatLargeCurrency($num) {
 
-	 if($num>1000) {
+	 if($num>10000) {
 
-        $x = round($num);
-        $x_number_format = number_format($x);
-        $x_array = explode(',', $x_number_format);
-        $x_parts = array('k', 'm', 'b', 't');
-        $x_count_parts = count($x_array) - 1;
-        $x_display = $x;
-        $x_display = $x_array[0] . ((int) $x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
-        $x_display .= $x_parts[$x_count_parts - 1];
+        return "$" . formatLargeNumber($num);
 
-        return "$" . $x_display;
-
+  } else {
+  	return "$" . number_format($num, 2);
   }
 
   return $num;
