29dab1055db23cd8870a3ceb9b5ac46baf572326
Matt Barba <matthewlawrencebarba@gmail.com>
2019-01-26 10:05:37 -0500

    More listing details clean up.

--- resources/views/listings/details/basics.blade.php
+++ resources/views/listings/details/basics.blade.php
@@ -5,47 +5,12 @@
 
 		<h4>Basic Details</h4>
 
-            <div class="col-sm-3 col-xs-6">
-              <div class="description-block">
-                <h5 class="description-header">{{ $listing->details->beds }}</h5>
-                <span class="description-text">Beds</span>
-              </div>
-            </div>
-
-            <div class="col-sm-3 col-xs-6">
-              <div class="description-block">
-                <h5 class="description-header">{{ $listing->details->baths }}</h5>
-                <span class="description-text">Baths</span>
-              </div>
-            </div>
-
-            <div class="col-sm-3 col-xs-6">
-              <div class="description-block">
-                <h5 class="description-header">{{ formatLargeCurrency($listing->details->price) }}</h5>
-                <span class="description-text">Price</span>
-              </div>
-            </div>
-
-            <div class="col-sm-3 col-xs-6">
-              <div class="description-block">
-                <h5 class="description-header">{{ $listing->details->livingArea }}</h5>
-                <span class="description-text">Sqft</span>
-              </div>
-            </div>
-
-            <div class="col-sm-3 col-xs-6">
-              <div class="description-block">
-                <h5 class="description-header">{{ $listing->details->purchaseType }}</h5>
-                <span class="description-text">Purchase Type</span>
-              </div>
-            </div>
-
-            <div class="col-sm-3 col-xs-6">
-              <div class="description-block">
-                <h5 class="description-header">{{ $listing->details->propertyType }}</h5>
-                <span class="description-text">Property Type</span>
-              </div>
-            </div>
+    {!! formatPropertyDetailsBlock( $listing->details->beds, 'Beds' ) !!}
+    {!! formatPropertyDetailsBlock( $listing->details->baths, 'Baths' ) !!}
+    {!! formatPropertyDetailsBlock( formatLargeCurrency($listing->details->price), 'Price' ) !!}
+    {!! formatPropertyDetailsBlock( $listing->details->livingArea, 'Sqft' ) !!}
+    {!! formatPropertyDetailsBlock( $listing->details->purchaseType, 'Purchase Type' ) !!}
+    {!! formatPropertyDetailsBlock( $listing->details->propertyType, 'Property Type' ) !!}
 		
 	</div>
 </div>
