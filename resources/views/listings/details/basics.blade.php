323f359279cda527e2b7cd3e522e46d30519be59
Matt Barba <matthewlawrencebarba@gmail.com>
2019-01-26 10:08:09 -0500

    more quick clean up

--- resources/views/listings/details/basics.blade.php
+++ resources/views/listings/details/basics.blade.php
@@ -1,16 +1,17 @@
 <div class="box box-default">
 	<div class="box-body">
 
-{{-- {{dd($listing)}} --}}
 
 		<h4>Basic Details</h4>
 
+    {!! formatPropertyDetailsBlock( $listing->details->purchaseType, 'Purchase Type' ) !!}
     {!! formatPropertyDetailsBlock( $listing->details->beds, 'Beds' ) !!}
     {!! formatPropertyDetailsBlock( $listing->details->baths, 'Baths' ) !!}
     {!! formatPropertyDetailsBlock( formatLargeCurrency($listing->details->price), 'Price' ) !!}
-    {!! formatPropertyDetailsBlock( $listing->details->livingArea, 'Sqft' ) !!}
-    {!! formatPropertyDetailsBlock( $listing->details->purchaseType, 'Purchase Type' ) !!}
+    <div class="row"></div>
     {!! formatPropertyDetailsBlock( $listing->details->propertyType, 'Property Type' ) !!}
+    {!! formatPropertyDetailsBlock( $listing->details->livingArea, 'Sqft' ) !!}
+    
 		
 	</div>
 </div>
@@ -44,6 +45,8 @@
     
 
     {!! formatPropertyDetailsBlock( $listing->details->YearBuilt, 'Year Built' ) !!}
+    {!! formatPropertyDetailsBlock( $listing->details->propertyTax, 'Monthly Property Tax' ) !!}
+    {!! formatPropertyDetailsBlock( $listing->details->AssociationFee, 'Monthly Assoc. Fee' ) !!}
 
   </div>
 </div>
