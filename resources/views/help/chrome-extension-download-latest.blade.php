3a37cb79076fa97bdfffd2184419dfb3baef4aeb
Matt Barba <matthewlawrencebarba@gmail.com>
2019-02-06 19:01:36 -0500

    Update for latest release.

--- resources/views/help/chrome-extension-download-latest.blade.php
+++ resources/views/help/chrome-extension-download-latest.blade.php
@@ -10,9 +10,19 @@
 
       <h3>Latest Version</h3>
 
-      <p>Here's a link to the latest version of the plugin. If you don't know how to install the plugin, here's a <a href="/chrome-extension-download-instructions">link to instructions</a></p>
+      <p>Here's a link to the latest version of the plugin. If you don't know how to install the plugin, here's a <a href="/chrome-extension-download-instructions">link to instructions</a>. Here's what's new:</p>
 
-      <p style="width: 400px"><a target="_blank" href="https://drive.google.com/file/d/1STaTZoPu1_BmWvs8K9tFpnQwfnELoWDE/view" type="submit" class="btn btn-primary btn-block btn-flat">Download New Version</a></p>
+      <ul>
+        <li>Rentals work on all supported websites: Zillow, Trulia and Realtor.com (not redfin)</li>
+        <li>Plugin will alert you when there's a new version available.</li>
+        <li>More useful linking back into the app for easier navigation of zip code / listing data.</li>
+        <li>Warning screen appears when listing can't be parsed rather than just failing silently.</li>
+        <li>Chrome plugin will now report back listing pages it can't parse so we can more easily see errors.</li>
+        <li>lots of little tweaks and bug fixes.</li>
+      </ul>
+
+      <p style="width: 400px"><a target="_blank" href="https://drive.google.com/file/d/1-he2Nh44MXjuDpwDhL75a9Gjd_EBBPzi/view?usp=sharing" type="submit" class="btn btn-primary btn-block btn-flat">Download New Version - v0.1.3</a></p>
+
 
   </div>
   <!-- /.box-body -->
@@ -26,7 +36,8 @@
       <h3>Older Builds</h3>
 
       <ul>
-        <li>0.1.1 - <a href="https://drive.google.com/file/d/1cxMA7CaZedZrViE1W8LOBckmFw0ExS-i/view?usp=sharing">Drive Download</a></li>
+        <li>0.1.2 - <a href="https://drive.google.com/file/d/1STaTZoPu1_BmWvs8K9tFpnQwfnELoWDE/view?usp=sharing">Drive Download</a> - For sale listings worked for all major portals, financial stats appeared, zip stats appeared.</li>
+        <li>0.1.1 - <a href="https://drive.google.com/file/d/1cxMA7CaZedZrViE1W8LOBckmFw0ExS-i/view?usp=sharing">Drive Download</a> - First shared version. Basic interface, only worked for redfin listings.</li>
       </ul>
 
   </div>
