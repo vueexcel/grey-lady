bf5f67b81e59ff4a73cbecf4c123fb3b9a2dbd1b
Matt Barba <matthewlawrencebarba@gmail.com>
2019-02-09 09:44:11 -0500

    Only show links to admins

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -50,35 +50,37 @@
             <li><a target=_Blank href="https://github.com/MatthewBarba/greylady-app/issues"><i class="fa fa-circle-o"></i>Report a bug</a></li>
           </ul>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class=""></i>
-            <span>Admin</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a target=_Blank href="https://my.apify.com/"><i class="fa fa-circle-o"></i>Apify Dashboard</a></li>
-            <li><a target=_Blank href="https://drive.google.com/drive/u/1/folders/1J6VbLYVkZNQegBFvPA6FnjT2WRyROlAt"><i class="fa fa-circle-o"></i>Google Drive Folder</a></li>
-            <li><a target=_Blank href="https://papertrailapp.com/systems/greyladyapp/events"><i class="fa fa-circle-o"></i>Hosted Logs</a></li>
-            <li><a target=_Blank href="/admin/"><i class="fa fa-circle-o"></i>Admin Panel</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class=""></i>
-            <span>CRUD Views</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="../listings"><i class="fa fa-circle-o"></i>Listings Index</a></li>
-            <li><a href="../zips"><i class="fa fa-circle-o"></i>Zips Index</a></li>
-            <li><a href="../watchlist"><i class="fa fa-circle-o"></i>Watchlists Index</a></li>
-          </ul>
-        </li>
+        @role('admin')
+          <li class="treeview">
+            <a href="#">
+              <i class=""></i>
+              <span>Admin</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a target=_Blank href="https://my.apify.com/"><i class="fa fa-circle-o"></i>Apify Dashboard</a></li>
+              <li><a target=_Blank href="https://drive.google.com/drive/u/1/folders/1J6VbLYVkZNQegBFvPA6FnjT2WRyROlAt"><i class="fa fa-circle-o"></i>Google Drive Folder</a></li>
+              <li><a target=_Blank href="https://papertrailapp.com/systems/greyladyapp/events"><i class="fa fa-circle-o"></i>Hosted Logs</a></li>
+              <li><a target=_Blank href="/admin/"><i class="fa fa-circle-o"></i>Admin Panel</a></li>
+            </ul>
+          </li>
+          <li class="treeview">
+            <a href="#">
+              <i class=""></i>
+              <span>CRUD Views</span>
+              <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="../listings"><i class="fa fa-circle-o"></i>Listings Index</a></li>
+              <li><a href="../zips"><i class="fa fa-circle-o"></i>Zips Index</a></li>
+              <li><a href="../watchlist"><i class="fa fa-circle-o"></i>Watchlists Index</a></li>
+            </ul>
+          </li>
+        @endrole
         
       </ul>
     </section><div class="slimScrollBar" style="background: rgb(0, 0, 0); width: 7px; position: absolute; top: -178.375px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 119.319px;"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
