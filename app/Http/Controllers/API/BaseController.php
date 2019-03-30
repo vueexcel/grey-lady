cf1c61e3be9fffe586279bea67aacb4b87612f85
Matt Barba <matthewlawrencebarba@gmail.com>
2019-03-30 13:05:41 -0400

    Ok. Way more of a pain in the ass

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -23,14 +23,7 @@ class BaseController extends Controller
             'message' => $message,
         ];
 
-        // header('Access-Control-Allow-Origin: *');
-        // header('Access-Control-Allow-Methods: *');
-        // header('Access-Control-Allow-Headers: *');
-
-        return response()->json($response, 200)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', '*');
+        return response()->json($response, 200);
     }
 
 
