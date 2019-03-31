d095e198d59e4a047292fbaaa865ff97ba74d12c
Matt Barba <matthewlawrencebarba@gmail.com>
2019-03-31 09:39:39 -0400

    fix link with prod version

--- app/Http/Controllers/API/SecureController.php
+++ app/Http/Controllers/API/SecureController.php
@@ -99,7 +99,7 @@ class SecureController extends BaseController
         $query = array();
         $current_chrome_plugin = array(
             'version' => '0.1.4',
-            'download_link' => 'http://greylady-app.test/chrome-extension-download-instructions/latest'
+            'download_link' => 'https://app.greyladyproject.com/chrome-extension-download-instructions/latest'
         );
 
 
