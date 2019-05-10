35661f45e8a6eeebf25cabf53e4e76ae41231d3c
Matt Barba <matthewlawrencebarba@gmail.com>
2019-05-10 09:13:56 -0400

    New getting started text

--- resources/views/help/getting-started.blade.php
+++ resources/views/help/getting-started.blade.php
@@ -11,22 +11,31 @@
 
       <h1>Welcome!</h1>
 
-      <h3>First, the purpose:</h3>
+      <h3>The purpose of GreyLady:</h3>
 
-      <p>This app is designed to help real estate investors find zip codes that could be attractive places to invest in so they don't need to manually research each one.</p>
+      <p>This app is designed to help real estate investors quickly analyze properties and zips that could be attractive places to invest in, so they don't waste time manually diligencing each one.</p>
 
-      <h3>Next, some very quick instructions on how to get started:</h3>
+      <p>The app is primarily driven by a chrome extension, aka the plugin. The plugin allows you to quickly review properties, easily add those properties and/or zip codes to the app, and then do some analytic and financial analysis on them.</p> 
 
-      <ul>
-        <li>Click around on the listing data collected zips in <a href="/explore?town=worcester">Worcester</a> and <a href="/explore?town=Boston">Boston</a>. Those are the only that work now. More to come in the future.</li>
-        <li><a href="/chrome-extension-download-instructions">Download the chrome plugin</a> (only works on Redfin).</li>
-      </ul>
-
-      <h3>Known Problems</h3>
+      <h3>Instructions on how to get started:</h3>
 
       <ul>
-        <li>A lot. Would help if you reported them as you found them... = )</li>
+        <li>What you will need...</li>
+        <ul>
+          <li><a href="https://www.google.com/chrome/">Google Chrome</a></li>
+          <li>The Chrome Plugin. Download the latest version - <a href="/plugin/latest">Here</a>. Instructions on installing the Plugin to your Chrome browser - <a href="/plugin/instructions">Here</a>.</li>
+          <li>Join our Slack Chat channel so you can give us feedback on the product. Message Bart to get an invite.</li>
+        </ul>
       </ul>
+
+
+      <h3>Known Problems:</h3>
+
+      <ul>
+        <li>A fair amount. Would help if you reported them as you found them... </li>
+      </ul>
+
+
 
     </div>
   </div>
