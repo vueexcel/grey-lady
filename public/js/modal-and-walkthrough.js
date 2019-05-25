7ef7bb543c89186346c6ac305e29781d908d1615
Matt Barba <matt@placester.com>
2019-05-24 20:21:17 -0400

    only show the modal when it's requested in the url.

--- public/js/modal-and-walkthrough.js
+++ public/js/modal-and-walkthrough.js
@@ -1,11 +1,18 @@
-$('#myModal').modal('show');
+if (window.location.search.indexOf('modal') > -1) {
+
+	$('#myModal').modal('show');
 
 
-$('#start-tour').on('click', function () {
+	$('#start-tour').on('click', function () {
 
-	$('#myModal').modal('hide');
-	introJs().start();
+		$('#myModal').modal('hide');
+		introJs().start();
 
-});
+	});
+
+
+}
+
+
 
   
