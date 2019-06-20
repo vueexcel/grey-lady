3cf106bafc49bdab1809d082c69f20aabe75411c
gulshan <vaibhav.excel2011@gmail.com>
2019-06-20 13:28:43 +0530

    update the scenario runner function

--- app/Http/Controllers/ScenarioController.php
+++ app/Http/Controllers/ScenarioController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use GuzzleHttp\Client as GuzzleHttpClient;
 use GuzzleHttp\Exception\RequestException;
 use Carbon\Carbon;
+use App\Models\Deals;
+use App\Models\Scenario;
 
 class ScenarioController extends Controller
 {
@@ -27,25 +29,28 @@ class ScenarioController extends Controller
 
         try {
 
-           $client = new GuzzleHttpClient();
-
-           $apiRequest = $client->request('GET', 'https://api.greyladyproject.com/api/v1/listings/' . $id, [
-                 'headers' => ['apiKey' => 'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ'],
+          $client = new GuzzleHttpClient();
+          $scenario_id = request()->get('scenario_id');
+          
+          $apiRequest = $client->request('GET', 'https://api.greyladyproject.com/api/v1/listings/' . $id, [
+            'headers' => ['apiKey' => 'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ'],
           ]);
           $response = json_decode($apiRequest->getBody()->getContents());
           $listing = $response[0];
 
           $zip = \App\Http\Helpers\ZipAPIHelper::getZipDetails($listing->details->location->zip);
-
-          // dd($listing);
-
-          // return view('listings.details.base', ['listing' => $listing, 'zip' => $zip]);
-          return view('scenario.run', ['listing' => $listing, 'zip' => $zip]);
+          $deal = Deals::where('listing_id', $id)->first();
+          $scenario = Scenario::where('id', $scenario_id)->where('deal_id', $deal->id)->first();
+          
+          return view('scenario.run', [
+            'listing' => $listing, 
+            'zip' => $zip, 
+            'deal' => $deal->toJson(), 
+            'scenario' => $scenario->toJson()
+          ]);
 
         } catch (RequestException $re) {
           dd($re);
         }
-
-        
     }
 }
